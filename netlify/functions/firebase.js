bd800de5cb9a44c710a688362fb830f39f300dd4
NickKelloggDDD <nicholas.mcdermott@kellogg.northwestern.edu>
2021-05-18 10:55:59 -0500

    commit

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -2,12 +2,12 @@ const firebase = require("firebase/app")
 require("firebase/firestore")
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGd7OAVUT1u3Sf9uoSh_6l8iHg2qh_JJ4",
-  authDomain: "kiei-451-spring-2021.firebaseapp.com",
-  projectId: "kiei-451-spring-2021",
-  storageBucket: "kiei-451-spring-2021.appspot.com",
-  messagingSenderId: "902466001150",
-  appId: "1:902466001150:web:b5a42e97ab21026b706daf"
+  apiKey: "AIzaSyCQhmxE-JDycCE9USqhBTHKICQSg7rGTLs",
+    authDomain: "kiei-451-60e69.firebaseapp.com",
+    projectId: "kiei-451-60e69",
+    storageBucket: "kiei-451-60e69.appspot.com",
+    messagingSenderId: "45804692184",
+    appId: "1:45804692184:web:4fecd079717baeafa5edad"
 } // replace
 
 if (!firebase.apps.length) {
